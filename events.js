1c42e889a53f23afb15b8853884b276f2e588445
Josh Jang <jangkeehyo@gmail.com>
2017-02-23 13:22:11 +0900

    implemented the expense page
    - add expense
    - remove expense
    - list up the all expenses

--- events.js
+++ events.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 // DB
 var mongojs = require('mongojs');
+var ObjectId = require('mongojs').ObjectId;
 var expense = mongojs('birthday', ['expenseDB']);
 var deposit = mongojs('birthday', ['depositDB']);
-var change = mongojs('birthday', ['changeDB']);
+var balance = mongojs('birthday', ['balanceDB']);
 
 router.get('/', function(req, res, next) {
     res.render('event.html');
@@ -24,6 +25,35 @@ router.post("/deposit", function(req, res) {
 
   expense.expenseDB.insert(req.body, function(err, docs) {
     res.json(docs);
+  });
+});
+
+router.post("/deposit/remove", function(req, res) {
+
+  console.log("req : ", req.body);
+  expense.expenseDB.remove({
+    _id : new ObjectId(req.body._id)
+  }, function(err, docs) {
+    res.json(docs);
+  });
+});
+
+router.get("/balance", function(req, res) {
+
+  balance.balanceDB.find(function(err, docs) {
+    res.json(docs[0].amount);
+  });
+});
+
+router.post("/balance", function(req, res) {
+  balance.balanceDB.update({
+    "_id" : "1"
+  }, {
+    $set: {
+      "amount" : req.body
+    }
+  }, function(err, docs) {
+    console.log(docs);
   });
 });
 
--- source/js/controllers/eventController.js
+++ source/js/controllers/eventController.js
@@ -1,9 +1,55 @@
 var eventApp = angular.module('eventApp', ["ngResource", "720kb.datepicker"]);
 
 eventApp.controller("EventCtrl", ['$scope', '$http', function($scope, $http){
+
+  $scope.balance = 0;
+
+  var refreshExpense = function() {
+    $http.get('events/deposit').success(function(response, data, status, headers, config) {
+      // console.log("I got the data I requested ");
+
+      $scope.days = response;
+    }).error(function(data, status, headers, config) {
+      console.log("expense error : ", status);
+    });
+  };
+
+  var countBalance = function (input, balance) {
+    // console.log("input : " + input + ". type is :" + typeof input);
+    // console.log("balance : " + balance + ". type is :" + typeof balance);
+    balance = parseInt(balance);
+    input = parseInt(input);
+    balance += input;
+    return balance;
+  };
+
+  var getBalance = function () {
+    $http.get('events/balance').success(function(response, data, status, headers, config) {
+      // console.log("getBalance() : ", response);
+
+      $scope.balance = response;
+    }).error(function(data, status, headers, config) {
+      console.log("expense error : ", status);
+    });
+  };
+
+  var updateBalance = function () {
+    $http.post('events/balance').success(function(response, data, status, headers, config) {
+      console.log("I got the data I requested ");
+
+      $scope.days = response;
+      console.log("res : ", response);
+    }).error(function(data, status, headers, config) {
+      console.log("expense error : ", status);
+    });
+  };
+  refreshExpense();
+  getBalance();
+
   $scope.expenseFormData = {
-    birthday_name : "알렉스",
-    expense_amount : "10,000"
+    expense_date : "",
+    birthday_name : "",
+    expense_amount : ""
   };
 
   $scope.depositFormData = {
@@ -12,34 +58,29 @@ eventApp.controller("EventCtrl", ['$scope', '$http', function($scope, $http){
     amount : "10,000"
   };
 
-  var data1 = {
-    "birthday_name" : "A",
-    "expense_amount" : "A"
-  };
-  var data2 = {
-    "name" : "C",
-    "amount" : "C"
-  };
-  $scope.days = [data1, data2];
+  $scope.days = [];
 
   $scope.submitExpenseForm = function() {
     $http.post('events/deposit', $scope.expenseFormData).success(function(response, data, status, headers, config) {
       // console.log("Data has been posted");
 
-      console.log("post response : ", response);
-    }). error(function(data, status, headers, config) {
+      console.log("countBalance() :" + countBalance($scope.expenseFormData.expense_amount, $scope.balance));
+      refreshExpense();
+      $scope.expenseFormData = null;
+    }).error(function(data, status, headers, config) {
       console.log("expense error : ", status);
     });
   };
 
-  var refreshExpense = function() {
-    $http.get('events/deposit').success(function(response, data, status, headers, config) {
-      console.log("I got the data I requested ");
+  $scope.removeExpense = function(item) {
+    console.log($scope.days[item]);
 
-      $scope.days = response;
-      console.log("res : ", response);
-    }). error(function(data, status, headers, config) {
+    $http.post('events/deposit/remove', $scope.days[item]).success(function(response, data, status, headers, config) {
+      console.log("response : ", response);
+      refreshExpense();
+    }).error(function(data, status, headers, config) {
       console.log("expense error : ", status);
     });
   };
+
 }]);
